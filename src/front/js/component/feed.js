8c392d7eeb7db13daec5aa488d2e1ca6966fc650
BryanAndresp <bryan_2904@hotmail.com>
2021-09-23 19:55:19 +0000

    INPROGRESS

--- src/front/js/component/feed.js
+++ src/front/js/component/feed.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function widgets() {
+export default function feed() {
     return (
         <div>
             
---
+++ src/front/js/component/sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sidebar() {
+	return (
+		<div className="sidebar">
+			<i className="fab fa-twitch" />
+		</div>
+	);
+}
+
+export default Sidebar;
