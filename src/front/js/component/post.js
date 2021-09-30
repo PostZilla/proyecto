016de4739d6fb15830908e0c4760e00f159f4a73
BryanAndresp <bryan_2904@hotmail.com>
2021-09-30 12:29:51 +0000

    INPROGRESS

--- src/front/js/component/post.js
+++ src/front/js/component/post.js
@@ -1,17 +1,15 @@
-import { text } from "@fortawesome/fontawesome-svg-core";
-import { Verified } from "@mui/icons-material";
+import { Avatar } from "@material-ui/core";
 import React from "react";
 import "../../styles/post.scss";
 
 function post() {
-	displayName;
-	username;
-	verified;
-	text;
-	img;
-	avatar;
-
-	return <div />;
+	return (
+		<div className="post">
+			<div className="post_avatar">
+				<Avatar src="https://i.pinimg.com/564x/a5/0c/64/a50c6419a7da56be403a445d5dc3f8d4.jpg" />
+			</div>
+		</div>
+	);
 }
 
 export default post;
