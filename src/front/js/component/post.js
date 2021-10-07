82dcbc24c04f20567f6b7db624c78edaa10671b5
BryanAndresp <bryan_2904@hotmail.com>
2021-10-07 20:16:11 +0000

    O

--- src/front/js/component/post.js
+++ src/front/js/component/post.js
@@ -25,13 +25,7 @@ function Post(props) {
 				<div className="post_header">
 					<div className="post_headerText">
 						<h3>
-<<<<<<< HEAD
-							<Link to={{ pathname: `/username/${props.userid}`, state: props.userid }}>
-								{props.name}
-							</Link>
-=======
 							<Link to={{ pathname: `${props.userid}`, state: props.userid }}>{props.name}</Link>
->>>>>>> main
 
 							<span className="post_headerSpecial Space">@{props.username}</span>
 							{!store.myFollower ? (
@@ -60,21 +54,12 @@ function Post(props) {
 				</div>
 				<div className="post_footer">
 					<ChatBubbleOutlineIcon fontSize="small" />
-<<<<<<< HEAD
-					<FavoriteBorderIcon
-						fontSize="small"
-						onClick={() =>
-							heart == undefined ? props.addLike(props.postid) : props.deleteLike(props.postid)
-						}
-					/>
-=======
 					<button
 						onClick={() =>
 							heart == undefined ? props.addLike(props.postid) : props.deleteLike(props.postid)
 						}>
 						<FavoriteBorderIcon fontSize="small" />
 					</button>
->>>>>>> main
 				</div>
 			</div>
 		</div>
