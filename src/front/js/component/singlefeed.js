0043de0f70664e1466bcaf8f4ed84d555d30c608
StefAranzazu <stefanyaranzazu1310@gmail.com>
2021-10-09 01:36:14 +0000

    .

--- src/front/js/component/singlefeed.js
+++ src/front/js/component/singlefeed.js
@@ -15,31 +15,54 @@ function SinglePost(props) {
 	}, []);
 
 	return (
-		<div className="post">
-			<div className="post_avatar">
-				<Link to={{ pathname: `/username/${props.userid}`, state: props.userid }}>
-					<Avatar src={props.profileimg} />
-				</Link>
+		<div className="profile">
+			<div className="profileRight">
+				<div className="profileRightTop">
+					<div className="profileCover">
+						<img
+							className="profileCoverImg"
+							src="https://i.pinimg.com/564x/a5/0c/64/a50c6419a7da56be403a445d5dc3f8d4.jpg"
+							alt=""
+						/>
+						<img
+							className="profileUserImg"
+							src="https://i.pinimg.com/564x/a5/0c/64/a50c6419a7da56be403a445d5dc3f8d4.jpg"
+							alt=""
+						/>
+					</div>
+					<div className="profileInfo">
+						<h4 className="profileInfoName">4Geeks</h4>
+						<span className="profileInfoDescription">Hola 4Geeks</span>
+					</div>
+				</div>
+				<div className="profileRightBottom" />
 			</div>
-			<div className="post_body">
-				<div className="post_header">
-					<div className="post_headerText">
-						<h3>
-							<Link to={{ pathname: `${props.userid}`, state: props.userid }}>{props.name}</Link>
+			<div className="post">
+				<div className="post_avatar">
+					<Link to={{ pathname: `/username/${props.userid}`, state: props.userid }}>
+						<Avatar src={props.profileimg} />
+					</Link>
+				</div>
+				<div className="post_body">
+					<div className="post_header">
+						<div className="post_headerText">
+							<h3>
+								<Link to={{ pathname: `${props.userid}`, state: props.userid }}>{props.name}</Link>
 
-							<span className="post_headerSpecial Space">@{props.username}</span>
-						</h3>
+								<span className="post_headerSpecial Space">@{props.username}</span>
+							</h3>
+						</div>
+						<div className="post_headerDescription">
+							<p>{props.text}</p>
+						</div>
 					</div>
-					<div className="post_headerDescription">
-						<p>{props.text}</p>
+					<div className="imgbox">
+						<img className="postimg" src={props.img} alt="" />
 					</div>
-				</div>
-				<div className="imgbox">
-					<img className="postimg" src={props.img} alt="" />
-				</div>
-				<div className="post_footer">
-					<ChatBubbleOutlineIcon fontSize="small" />
-					<FavoriteBorderIcon className="favorite" onClick={() => actions.addLike(props.postid)} />
+					<div className="post_footer">
+						<ChatBubbleOutlineIcon fontSize="small" />
+						<FavoriteBorderIcon className="favorite" onClick={() => actions.addLike(props.postid)} />
+					</div>
 				</div>
 			</div>
 		</div>
