b72d5e388f9753f9e840956e89d4d5f989295f3f
StefAranzazu <stefanyaranzazu1310@gmail.com>
2021-10-15 10:23:20 +0000

    arreglos

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,13 +4,24 @@ import Feed from "../component/feed";
 import Sidebar from "../component/sidebar";
 import Widgets from "../component/widgets";
 import { Context } from "../store/appContext";
+import { useHistory } from "react-router-dom";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
+	const history = useHistory();
 
-	useEffect(() => {
-		actions.getUser();
-	}, []);
+	useEffect(
+		() => {
+			if (store.isAuthenticate == false) {
+				history.goBack();
+				alert("Vuelve a iniciar sesion");
+			}
+		},
+		[store.isAuthenticate]
+	),
+		useEffect(() => {
+			actions.getUser();
+		}, []);
 
 	return (
 		<div className="App">
